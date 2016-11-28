d8561d7b9dea5067a58e3618d67f1a7b60395210
Luis A. Rivas <rivasl.works@gmail.com>
2016-11-28 10:43:33 -0430

    Añadido formulario para incluir una noticia: Se trabajó con Blade, controladores resource, csrf_field

---
+++ resources/views/layouts/formulario.blade.php
@@ -0,0 +1,21 @@
+<form role='form' method="POST" action="{{url('noticias')}}">
+  <div class="form-group">
+  	{{ csrf_field() }}
+    <label for="titulo">Título</label>
+    <input type="text" class="form-control" name="titulo" placeholder="título">
+  </div>
+  <div class="form-group">
+    <label for="descripcion">Descripción</label>
+    <textarea type="text" class="form-control" name="descripcion" placeholder="descripcion"></textarea>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputFile">File input</label>
+    <input type="file" id="exampleInputFile">
+    <p class="help-block">Example block-level help text here.</p>
+  </div>
+  <div class="form-group">
+    <label for="urlImg">Imagen</label>
+    <input type="text" class="form-control" name="imagen" placeholder="imagen"></input>
+  </div>
+  <button type="submit" class="btn btn-primary">Crear</button>
+</form>
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+// Route::get('/noticias', 'NoticiasController@index');
+Route::resource('/noticias', 'NoticiasController');
