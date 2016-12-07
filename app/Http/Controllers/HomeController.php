1f19323cb190b1c376707e3a8c9b9a454dd64029
Luis A. Rivas <rivasl.works@gmail.com>
2016-12-07 12:31:09 -0430

    Renderización de los datos contenidos en la tabla de noticias

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Noticia;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $noticias = Noticia::all();
+        return view('home')->with(['noticias' => $noticias]);
     }
 }
--- resources/views/layouts/tabla.blade.php
+++ resources/views/layouts/tabla.blade.php
@@ -1,3 +1,20 @@
 <table class="table table-hover">
-  ...
+	@if(isset($noticias))
+		<thead>
+			<th>Título</th>
+			<th>Descripción</th>
+			<th>Imagen</th>
+		</thead>
+		<tbody>
+		@foreach($noticias as $n)
+			<tr>titulo
+				<td>{{ $n->titulo }}</td>
+				<td>{{ $n->descripcion }}</td>
+				<td>
+					<img src="imgNoticias/{{ $n->urlImg }}" class="img-responsive" alt="Responsive image">
+				</td>
+			</tr>
+		@endforeach		
+		</tbody>
+	@endif
 </table>
